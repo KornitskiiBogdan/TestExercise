4f4953554af3ee32aaf91b88326fdf4ebe9cad43
Богдан Корницкий <bogdan.kornickiy.00@mail.ru>
2023-07-08 22:18:06 +0700

    Добавлен проект. Добавлен конвертер

---
+++ TestExercise/EOperationsToString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TestExercise
+{
+    public class EOperationsToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Operations eOperations = (Operations)value;
+            if (eOperations == Operations.Add)
+            {
+                return "Добавлено";
+            }
+            else
+            {
+                return "Удалено";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
