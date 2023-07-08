4454d7993a15865548d667267a0ae2e16b10ff90
Богдан Корницкий <bogdan.kornickiy.00@mail.ru>
2023-07-08 18:04:24 +0700

    Удален лишний класс. Добавлено еще одно окно

---
+++ TestExercise/GiveAwayMoneyWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestExercise
+{
+    /// <summary>
+    /// Interaction logic for GiveAwayMoneyWindow.xaml
+    /// </summary>
+    public partial class GiveAwayMoneyWindow : Window
+    {
+        public GiveAwayMoneyWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sumBox_TextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void sumBox_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
